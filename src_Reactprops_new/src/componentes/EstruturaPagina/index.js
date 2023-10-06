8c3c8658444c2b3d01d6d38671303319265fa494
Filipe Alves <lipeqa@gmail.com>
2023-10-06 13:20:36 -0300

    adicioando projeto reactprops_new

---
+++ src_Reactprops_new/src/componentes/EstruturaPagina/index.js
@@ -0,0 +1,16 @@
+import Rodape from '../Rodape';
+import Topo from '../Topo';
+
+const EstruturaPagina = ( props ) => {
+  return (
+    <section>
+      <Topo />
+
+      { props.children }
+
+      <Rodape />
+    </section>
+  );
+};
+
+export default EstruturaPagina;
--- src_Reactprops_new/src/paginas/Contato/index.js
+++ src_Reactprops_new/src/paginas/Contato/index.js
@@ -1,58 +1,71 @@
-import React from 'react';
+import Mapa from "../../componentes/Mapa";
 
-import Topo from '../../componentes/Topo';
-import Rodape from '../../componentes/Rodape';
-import Mapa from '../../componentes/Mapa';
+import "./estilo.css";
 
-import './estilo.css';
+import EstruturaPagina from "../../componentes/EstruturaPagina";
 
 const Contato = () => {
   return (
-    <section>
-      <Topo />
-
+    <EstruturaPagina>
       <div className="contato-container">
         <div className="contato-mapa">
-          <h2 className="titulo-sessao">Horários de <b>atendimento</b></h2>
+          <h2 className="titulo-sessao">
+            Horários de <b>atendimento</b>
+          </h2>
 
           <p>Agende uma consulta pelo telefone: (21) 3699 - 9999</p>
 
           <ul className="lista-medicos">
             <li>
-              <img className="img-aparelho" src="assets/medico01.png" alt="Dr. Hélio" title="Dr. Hélio"/>
+              <img
+                className="img-aparelho"
+                src="assets/medico01.png"
+                alt="Dr. Hélio"
+                title="Dr. Hélio"
+              />
               <p>Dr. Hélio</p>
               <p>Segundas, quartas e sextas das 09:00 às 16:00</p>
             </li>
 
             <li>
-              <img className="img-aparelho" src="assets/medico02.png" alt="Dr. Carlos" title="Dr. Carlos"/>
+              <img
+                className="img-aparelho"
+                src="assets/medico02.png"
+                alt="Dr. Carlos"
+                title="Dr. Carlos"
+              />
               <p>Dr. Carlos</p>
               <p>Terças e quintas das 13:00 às 18:00</p>
             </li>
 
             <li>
-              <img className="img-aparelho" src="assets/medico03.png" alt="Dra. Suzana" title="Dra. Suzana"/>
+              <img
+                className="img-aparelho"
+                src="assets/medico03.png"
+                alt="Dra. Suzana"
+                title="Dra. Suzana"
+              />
               <p>Dra. Suzana</p>
               <p>Terças, quintas e sábados das 08:00 às 12:00</p>
             </li>
           </ul>
-
         </div>
-        
-        <div className="contato-mapa">
-          <h2 className="titulo-sessao">Onde estamos <b>localizados</b>?</h2>
 
-          <p>Av. Ayrton Senna, 3000 - Barra da Tijuca, Rio de Janeiro - RJ. CEP: 22775-904
+        <div className="contato-mapa">
+          <h2 className="titulo-sessao">
+            Onde estamos <b>localizados</b>?
+          </h2>
+
+          <p>
+            Av. Ayrton Senna, 3000 - Barra da Tijuca, Rio de Janeiro - RJ. CEP:
+            22775-904
           </p>
 
           <Mapa />
         </div>
-
       </div>
-
-      <Rodape />
-    </section>
+    </EstruturaPagina>
   );
-}
+};
 
 export default Contato;
